1526b2ca3aea46ea00fc0942894a6feb42ffbfe2
Angelo Karugo <angelo@myhealthafrica.com>
2021-09-27 11:23:26 +0300

    debugging telemedicine payment handlers

--- operation/flutterwavePayment.php
+++ operation/flutterwavePayment.php
@@ -1,6 +1,6 @@
 <?php
 if (
-    !isset($_POST["rave"])
+    !isset($_POST["wave"])
     || !isset($_POST["appointment_id"])
 ) {
     die(json_encode(["error" => "missing request data", "error_code" => 1]));
@@ -9,4 +9,4 @@ if (
 require "include/DB_Functions.php";
 $db = new DB_Functions();
 
-die(json_encode($db->processFlutterwavePayment($_POST["appointment_id"], $_POST["rave"])));
+die(json_encode($db->processFlutterwavePayment($_POST["appointment_id"], $_POST["wave"])));
