f1654e2185c9a983d68e4de40a75d89dab367b97
Angelo Karugo <angelo@myhealthafrica.com>
2021-10-06 12:36:14 +0300

    error pop-up for failed payment

--- booking.php
+++ booking.php
@@ -20,6 +20,7 @@
     <link href="css/responsive.css" rel="stylesheet" type="text/css" />
     <link rel="stylesheet" type="text/css" href="assets/bundles/bootstrap-timepicker/css/bootstrap-timepicker.css" />
     <link rel="stylesheet" type="text/css" href="assets/bundles/bootstrap-datepicker/css/bootstrap-datepicker.css" />
+    <link rel="stylesheet" type="text/css" href="https://www.myhealthafrica.com/myonemedpro/assets/bundles/sweet-alert/sweetalert.min.css" />
     <link href="css/booking.css?v=<?php echo $inclusions_version ?>" rel="stylesheet" type="text/css" />
     <!-- favicon -->
     <link rel="shortcut icon" href="images/my-health-africa.png" />
